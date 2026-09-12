a00dc46e2394d435cb417f297a8598b2506147b4
Daniel-Ajila <dajila10@utmachala.edu.ec>
2026-09-11 23:35:38 -0500

    Conexion a supaBase definitiva

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Reemplaza estas dos cadenas con tus credenciales reales de Supabase
-const supabaseUrl = "https://tu-proyecto.supabase.co"; 
+const supabaseUrl = "https://dukutyembduduwlotcda.supabase.co"; 
 const supabaseAnonKey = "sb_publishable_NV2_2t8Nd9zqASJva75-Ug_7J6xuR0r";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
